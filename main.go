24d87269f6d1749d9115e2ed3782e383a15c42d9
Ruslan <noname0263@gmail.com>
2024-09-08 12:17:54 +0900

    init

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	out := os.Stdout
+	if !(len(os.Args) == 2 || len(os.Args) == 3) {
+		panic("usage go run main.go . [-f]")
+	}
+	path := os.Args[1]
+	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
+	err := dirTree(out, path, printFiles)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func dirTree(out io.Writer, path string, printFiles bool) error {
+	dirs, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("dirs was broken")
+	}
+	fmt.Println(dirs)
+	return nil
+}
