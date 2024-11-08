f70d3c5ef33f70c657cc0d1a3a03b5eb593d0331
Ruslan <noname0263@gmail.com>
2024-11-08 23:59:09 +0900

    init

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"os"
+	"sort"
+	"strconv"
 )
 
 func main() {
@@ -20,26 +21,95 @@ func main() {
 }
 
 func dirTree(out io.Writer, path string, printFiles bool) error {
-	err := getTree(&out, path, printFiles)
-	if err != nil {
-		return err
-	}
-	return nil
+	err := getTree(out, path, printFiles, "")
+	return err
 }
-func getTree(out *io.Writer, path string, printFiles bool) error {
-	arr, err := os.ReadDir(path)
+
+func getTree(out io.Writer, path string, printFiles bool, prefix string) error {
+	entries, err := os.ReadDir(path)
 	if err != nil {
 		return err
 	}
-	for _, obj := range arr {
-		if obj.IsDir() {
-			err := getTree(out, path+"/"+obj.Name(), printFiles)
+	if !printFiles {
+		n := 0
+		for _, entry := range entries {
+			if entry.IsDir() {
+				entries[n] = entry
+				n++
+			}
+		}
+		entries = entries[:n]
+	}
+
+	sort.Slice(entries, func(i, j int) bool { return entries[i].Name() < entries[j].Name() })
+
+	for i := 0; i < len(entries)-1; i++ {
+		if entries[i].IsDir() {
+			if _, err := out.Write([]byte(prefix + "├───" + entries[i].Name() + "\n")); err != nil {
+				return err
+			}
+
+			if err = getTree(out, path+string(os.PathSeparator)+entries[i].Name(), printFiles, prefix+"│\t"); err != nil {
+				return err
+			}
+
+		} else if printFiles {
+			fileInfo, err := os.Stat(path + string(os.PathSeparator) + entries[i].Name())
 			if err != nil {
 				return err
 			}
-		} else {
-			fmt.Println(obj.Name())
+
+			fileSize := fileInfo.Size()
+			var strFileSize string
+			if fileSize == 0 {
+				strFileSize = "empty"
+			} else {
+				strFileSize = strconv.FormatInt(fileSize, 10) + "b"
+			}
+
+			_, err = out.Write([]byte(prefix + "├───" + entries[i].Name() + " (" + strFileSize + ")" + "\n"))
+			if err != nil {
+				return err
+			}
 		}
 	}
+	last := len(entries) - 1
+	if last < 0 {
+		return nil
+	}
+	if entries[last].IsDir() {
+		_, err := out.Write([]byte(prefix + "└───" + entries[last].Name() + "\n"))
+		if err != nil {
+			return err
+		}
+		if err = getTree(out, path+string(os.PathSeparator)+entries[last].Name(), printFiles, prefix+"\t"); err != nil {
+			return err
+		}
+	} else if printFiles {
+		fileInfo, err := os.Stat(path + string(os.PathSeparator) + entries[last].Name())
+		if err != nil {
+			return err
+		}
+
+		fileSize := fileInfo.Size()
+		var strFileSize string
+		if fileSize == 0 {
+			strFileSize = "empty"
+		} else {
+			strFileSize = strconv.FormatInt(fileSize, 10) + "b"
+		}
+
+		_, err = out.Write([]byte(prefix + "└───" + entries[last].Name() + " (" + strFileSize + ")" + "\n"))
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
+
+/*
+├───
+└───
+│
+*/
